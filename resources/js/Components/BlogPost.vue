777ede48de53d6d8948739daeea07be1a9c5b007
Justin Struk <10167488+JStruk@users.noreply.github.com>
2024-03-18 14:45:53 -0400

    refactor the UI to use daisy UI

---
+++ resources/js/Components/BlogPost.vue
@@ -0,0 +1,33 @@
+<script setup>
+const props = defineProps({
+    blogPost: {
+        type: Object,
+        required: true
+    }
+})
+</script>
+
+<template>
+    <div class="card bg-base-200 shadow-xl">
+        <div class="card-body">
+            <div class="flex justify-between">
+                <div>
+                    <h2 class="card-title">{{ blogPost.title }}</h2>
+                </div>
+                <div>
+                    <p class="">{{ blogPost.author }}</p>
+                </div>
+            </div>
+            <p class="italic">{{ blogPost.subtitle }}</p>
+            <p class="text-gray-700">
+                {{ blogPost.created_diff_for_humans }}
+            </p>
+            <hr class="h-px my-8 bg-gray-300 border-0 dark:bg-gray-400">
+            <p class="pt-4 truncate max-h-72" v-html="blogPost.contents"></p>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,8 +1,9 @@
 <!DOCTYPE html>
-<html>
+<html data-theme="dark" lang="html">
     <head>
         <meta charset="utf-8"/>
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0"/>
+        <title>Blog - Justin Struk</title>
         @vite('resources/js/app.js')
         @inertiaHead
     </head>
